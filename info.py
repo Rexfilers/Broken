53046eaf14a139846b422c84210e6f234dc45462
Rexfilers <138610025+Rexfilers@users.noreply.github.com>
2023-10-07 15:49:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'Xd15uOuu9GNbEIrViOvr3dEQsLn1')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'yd4YtVtct2PX4UFY66QoKgKKFc82')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
